a71d47e1683046a4c260bc0f2020673fc5669525
Ifrim Silviu <ifrim.silviu2@gmail.com>
2023-02-26 14:28:47 +0200

    Create queryClient.

---
+++ src/request/config/queryClient.js
@@ -0,0 +1,17 @@
+import { QueryClient } from "react-query";
+import axiosInstance from "./axios";
+
+const pathSeparator = "/";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: ({ queryKey }) =>
+        axiosInstance
+          .get(queryKey.join(pathSeparator))
+          .then(({ data }) => data),
+    },
+  },
+});
+
+export default queryClient;
