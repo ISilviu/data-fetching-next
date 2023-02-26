55eb37fcbff8befb025f5438d7bfb33a3d3a26c4
Ifrim Silviu <ifrim.silviu2@gmail.com>
2023-02-26 13:35:17 +0200

    Create movies api.

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,42 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const movies = [
+  {
+      "id": 1,
+      "title": "Heat",
+      "year": "1995",
+      "runtime": "170",
+      "genres": [
+          "Action",
+          "Crime",
+          "Drama"
+      ],
+      "director": "Michael Mann",
+  },
+  {
+      "id": 2,
+      "title": "L.A. Confidential",
+      "year": "1997",
+      "runtime": "138",
+      "genres": [
+          "Crime",
+          "Drama",
+          "Mystery"
+      ],
+      "director": "Curtis Hanson",
+  },
+  {
+    "id": 3,
+      "title": "Blood Diamond",
+      "year": "2006",
+      "runtime": "143",
+      "genres": [
+          "Adventure",
+          "Drama",
+          "Thriller"
+      ],
+      "director": "Edward Zwick",
+  },
+];
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(movies);
 }
