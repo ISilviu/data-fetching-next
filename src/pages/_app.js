4900a69b192a50081bba522cccb3eac4974da712
Ifrim Silviu <ifrim.silviu2@gmail.com>
2023-03-05 18:34:31 +0200

    Add hydration configuration.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,22 @@
+import React from "react";
 import "@/styles/globals.css";
-import { QueryClientProvider } from "@tanstack/react-query";
-import queryClient from "../request/config/queryClient";
+import queryClientConfiguration from "@/request/config/queryClient";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }) {
+  const [queryClient] = React.useState(
+    () => new QueryClient(queryClientConfiguration)
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
     </QueryClientProvider>
   );
 }
